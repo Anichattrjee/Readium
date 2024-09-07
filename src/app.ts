9ae587d12a4195a9550aa6efe77ce259fe125657
Aniruddha Chatterjee <anichat1999@gmail.com>
2024-09-07 18:49:35 +0530

    Frontend Initial Project Setup & ShadCn-UI configuration done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { NextFunction, Request, Response } from "express";
-import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/user.route";
